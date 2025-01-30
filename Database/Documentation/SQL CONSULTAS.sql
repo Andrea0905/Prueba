c24476b8473a361676445f05eac118460bc81651
Andrea0905 <andysgarcia0509@gmail.com>
2025-01-30 12:03:15 -0600

    Prueba nueva

---
+++ Database/Documentation/SQL CONSULTAS.sql
@@ -0,0 +1,60 @@
+SELECT * FROM [dbo].[Person]
+
+
+SELECT *FROM [dbo].[Person]
+WHERE PersonID = 1
+
+-- ON-PREMISES
+
+--Crud
+INSERT INTO [dbo].[Person] ([Name]) VALUES('Teresa')
+
+--Crud se pondeia a todos porque no se especifica a quien se le cambia el nombre
+UPDATE [dbo].[Person] SET 
+[Name] ='Perla'
+
+---
+UPDATE [dbo].[Person] SET 
+[Name] ='Andrea'
+WHERE PersonID = 1
+
+UPDATE [dbo].[Person] SET 
+[Name] ='Adalid'
+WHERE PersonID = 2
+
+UPDATE [dbo].[Person] SET 
+[Name] ='Daniel'
+WHERE PersonID = 3
+
+UPDATE [dbo].[Person] SET 
+[Name] ='Maria'
+WHERE PersonID = 6
+
+--Crud
+
+DELETE FROM [dbo].[Person]
+WHERE PersonID = 1
+
+
+-- INNER Joins
+SELECT * FROM [dbo].[Items] i 
+
+SELECT * FROM [dbo].[Person] p
+
+SELECT p.Name, i.Completed FROM [dbo].[Items] i 
+INNER JOIN [dbo].[Person] p ON i.PersonID = p.PersonID
+
+SELECT * FROM [dbo].[Person] p
+SELECT * FROM [dbo].[Items] i 
+
+SELECT p.Name, i.Completed FROM [dbo].[Person] p
+INNER JOIN [dbo].[Items] i ON p.PersonID = i.PersonID
+
+-- Left Joins
+SELECT p.Name, i.Completed FROM [dbo].[Person] p
+LEFT JOIN [dbo].[Items] i ON p.PersonID = i.PersonID
+
+-- Tipos de indexes
+--   Index Cluster -> Primary Key
+--   Non Cluster Index --> Indices adicionales
+--Termino la clase 1112
