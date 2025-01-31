9e5cde3bb2b900d6f33b427cf5b8bbec6e9e1e67
Andrea0905 <andysgarcia0509@gmail.com>
2025-01-31 14:41:17 -0600

    Pruebita

--- Database/Queries/SQL identificacion.sql
+++
@@ -1,5 +0,0 @@
--- SQL (Structured Query Language): 
---     Transact-SQL (Microsoft)
---     DML (Data Manipulation Language): CRUD(Create -> INSERT, Read -> SELECT, Update -> UPDATE, Delete > Delete)
---     DDL (Data Definition Language): Create (Database, Table)
---     PL/SQL (Oracle)
